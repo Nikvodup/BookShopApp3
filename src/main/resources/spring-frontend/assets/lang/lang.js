53cb783454ea4d061fc6ed1f82179e2de5c6a461
codevarius <koshaevk@gmail.com>
2021-04-01 16:18:49 +0300

    fix .html fragments and lang.js

--- src/main/resources/spring-frontend/assets/lang/lang.js
+++ src/main/resources/spring-frontend/assets/lang/lang.js
@@ -1,10 +1,8 @@
-
-
-    $(document).ready(function (){
-    $("#locales").change(function (){
-        var selectedOption=$("#locales").val();
-        if(selectedOption!='') {
-            window.location.replace('?lang' + selectedOption);
+$(document).ready(function () {
+    $("#locales").change(function () {
+        var selectedOption = $("#locales").val();
+        if (selectedOption != '') {
+            window.location.replace('?lang=' + selectedOption);
         }
     });
 });
