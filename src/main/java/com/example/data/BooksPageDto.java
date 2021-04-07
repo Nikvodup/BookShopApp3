14025bc81128005b70061ed2433baf25c1d904dc
Alex <ransbach25@gmail.com>
2021-04-07 19:15:35 +0500

    BookShop

--- src/main/java/com/example/data/BooksPageDto.java
+++ src/main/java/com/example/data/BooksPageDto.java
@@ -1,7 +1,30 @@
 package com.example.data;
 
-import org.springframework.http.ResponseEntity;
+import java.util.List;
 
 public class BooksPageDto {
 
+    private Integer count;
+    private List<Book> books;
+
+    public BooksPageDto(List<Book> books) {
+        this.books = books;
+        this.count = books.size();
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
